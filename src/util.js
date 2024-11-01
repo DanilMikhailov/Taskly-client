d8ce5c0b25843c0664d4a3bcd7a50194ef01c93f
DanilMikhailov <78970984+DanilMikhailov@users.noreply.github.com>
2024-11-01 21:51:14 +0500

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,22 +1,17 @@
-export const API_BASE_URL = "https://taskly-server-production-e0c1.up.railway.app:8080";
+export const API_BASE_URL = "taskly-server-production-e0c1.up.railway.app:8080/api/v1";
+import { useState, useEffect } from "react"; 
 export const useLocalStorage = (key, defaultValue) => { 
     const [value, setValue] = useState(() => { 
-        try {
-            const currentValue = localStorage.getItem(key); 
-            return currentValue ? JSON.parse(currentValue) : defaultValue; 
-        } catch (error) {
-            console.error("Ошибка при извлечении данных из localStorage", error);
+        const currentValue = localStorage.getItem(key); 
+        if (currentValue) { 
+            return JSON.parse(currentValue) 
+        } 
+        else { 
             return defaultValue; 
-        }
+        } 
     }); 
-
     useEffect(() => { 
-        try {
-            localStorage.setItem(key, JSON.stringify(value)); 
-        } catch (error) {
-            console.error("Ошибка при сохранении данных в localStorage", error);
-        }
+        localStorage.setItem(key, JSON.stringify(value)); 
     }, [value]); 
-
     return [value, setValue]; 
-};
+}; 
