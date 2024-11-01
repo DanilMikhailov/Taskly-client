e87b4a360e685b196eb629d334c85d223c024723
DanilMikhailov <78970984+DanilMikhailov@users.noreply.github.com>
2024-11-01 21:46:23 +0500

    Update util.js

--- src/util.js
+++ src/util.js
@@ -1,3 +1,4 @@
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const useLocalStorage = (key, defaultValue) => { 
     const [value, setValue] = useState(() => { 
         try {
